68dcb2f53d6ddf8fd91e4b18988f94ae07019a13
federicaarosio <arosio.fede@gmail.com>
2024-02-20 16:42:20 +0100

    added components, removed default style, added scss and bootstrap

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <header>
+        <pre>
+            Header
+        </pre>
+    </header>
+</template>
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <main>
+        <h1>
+            Projects
+        </h1>
+    </main>
+</template>
+<script>
+export default {
+    name: 'AppMain',
+}
+</script>
+<style lang="">
+    
+</style>
